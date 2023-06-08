b45913ef5e54e7d7a4ee0b7e39d52bb1b9340afc
Vinicius Janner <vinicius.janner.vortigo@superdigital.com.br>
2023-06-07 21:13:13 -0300

    Init project.

---
+++ app/src/main/java/com/viniciusjanner/android_github_actions/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.viniciusjanner.android_github_actions
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
