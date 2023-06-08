ff151ad9a2b59078ce9c1bdb0be380310a832619
Vinicius Janner <vinicius.janner.vortigo@superdigital.com.br>
2023-06-07 21:42:18 -0300

    Migrate Gradle Groovy to Kotlin DSL.

--- app/src/main/java/com/viniciusjanner/android_github_actions/MainActivity.kt
+++ app/src/main/java/com/viniciusjanner/android_github_actions/MainActivity.kt
@@ -2,8 +2,10 @@ package com.viniciusjanner.android_github_actions
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.viniciusjanner.android_ci_github_actions.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
