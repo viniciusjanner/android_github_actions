1e25b1d064e30213a99a2d4d99af8d7f65420442
Vinicius Janner <viniciusjanner@gmail.com>
2023-08-31 00:47:32 -0300

    Rename activity (MainActivity to HomeActivity).

--- app/src/main/java/com/viniciusjanner/android_github_actions/HomeActivity.kt
+++ app/src/main/java/com/viniciusjanner/android_github_actions/HomeActivity.kt
@@ -3,7 +3,7 @@ package com.viniciusjanner.android_github_actions
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
