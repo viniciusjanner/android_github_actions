637596ab4a9790da3e85286f96abb28a4e500c63
Vinicius Janner <viniciusjanner@gmail.com>
2023-09-06 19:47:52 -0300

    Implements class DataStoreManager in class App.

---
+++ app/src/main/java/com/viniciusjanner/android_github_actions/App.kt
@@ -0,0 +1,39 @@
+package com.viniciusjanner.android_github_actions
+
+import android.app.Application
+import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
+import com.viniciusjanner.android_github_actions.prefs.DataStoreManager
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+class App : Application() {
+
+    companion object {
+        lateinit var dataStoreManager: DataStoreManager
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        dataStoreManager = DataStoreManager(this@App)
+
+        val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+            Log.e("coroutineExceptionHandler", "throwable = ${throwable.message}")
+            throwable.printStackTrace()
+        }
+
+        val coroutineContext: CoroutineContext = Dispatchers.Default + coroutineExceptionHandler
+
+        CoroutineScope(coroutineContext).launch {
+            dataStoreManager.getTheme().collect {
+                AppCompatDelegate.setDefaultNightMode(
+                    if (it) AppCompatDelegate.MODE_NIGHT_YES
+                    else AppCompatDelegate.MODE_NIGHT_NO
+                )
+            }
+        }
+    }
+}
